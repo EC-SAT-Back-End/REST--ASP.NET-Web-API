7ad31ea88c97a5ddf548e0318d7b8997b9894f8f
Ahmed Break <Ahmed.Break@ec-sat.local>
2021-01-27 13:13:46 +0200

    Add project files.

---
+++ REST -ASP.NET-Web-API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace REST__ASP.NET_Web_API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ REST -ASP.NET-Web-API/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace REST__ASP.NET_Web_API.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
